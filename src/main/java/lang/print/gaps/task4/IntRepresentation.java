fc1f5f04e2b5956f1a04957879b5e05cf8d255b7
Sergey234 <mostov.sergey88@gmail.com>
2022-10-20 18:39:04 +0300

    Module 3. Answer task 4;

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int symbolOne = 'c'; // 99 'c'
+        int symbolTwo = 'h'; // 104 'h'
+        int symbolThree = 'a'; // 97 'a'
+        int symbolFour = 'r'; // 114 'r'
     }
 }
